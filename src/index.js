929260e3255ba2da889d6e9e4510a442aa7b9599
dilip baghel <dilip.18baghel@gmail.com>
2024-01-29 12:00:24 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import dotenv from "dotenv";
 import connectDB from "./db/db_connect.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("app is failed in running", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("app is running on port : ", process.env.PORT);
+    });
+  })
+
+  .catch((err) => {
+    console.log("MongoDB connection failed !!!", err);
+  });
